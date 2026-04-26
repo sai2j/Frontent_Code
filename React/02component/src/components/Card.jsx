af4234ee23ceffe33782160e2d83502a30655b2f
sai2j <sahilkolhe089@gmail.com>
2026-04-26 10:00:00 +0530

    reactjs component practise code

---
+++ React/02component/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>
+          <h1> hello,guys</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+            laborum iure excepturi Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, error?lorem3
+            Lorem, dolor.
+          </p>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default card;
